34ddf3e0c47d0a62af5d90f38a3631e65b355788
Richard Cho <richardcho100@gmail.com>
2020-01-17 19:00:03 -0500

    Add fetchAndLoadBooks as instance method & invoke in  constructor function

--- src/components/books.js
+++ src/components/books.js
@@ -2,5 +2,10 @@ class Books {
     constructor() {
         this.books = []
         this.adapter = new BooksAdapter()
+        this.fetchAndLoadBooks()
+    }
+
+    fetchAndLoadBooks() {
+        this.adapter.getBooks().then(notes => console.log(notes))
     }
 }
