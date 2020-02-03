06e34a45e1671d6f1b43024654125d7bdcea978b
Richard Cho <richardcho100@gmail.com>
2020-02-03 16:28:46 -0500

    Add initBindingsAndEventListeners & createUser method

--- src/components/users.js
+++ src/components/users.js
@@ -3,4 +3,14 @@ class Users {
         this.users = []
     }
 
+    initBindingsAndEventListeners() {
+        this.signupForm = document.querySelector(".signup-form")
+        this.signupForm.addEventListener('submit', this.createUser);
+    }
+
+    createUser(e) {
+        e.preventDefault();
+        console.log('User is being created')
+    }
+
 }
