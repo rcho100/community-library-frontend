fc9568906951754c94c3ce14ac52455a143ac926
Richard Cho <richardcho100@gmail.com>
2020-02-03 16:57:13 -0500

    Add loginUser method to post fetch signupInfo and get response

--- src/adapters/UsersAdapter.js
+++ src/adapters/UsersAdapter.js
@@ -2,4 +2,22 @@ class UserAdapter {
     constructor() {
         this.baseURL = "http://localhost:3000/signup"
     }
+
+    loginUser(signupInfo) {
+        const configurationObject = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                body: JSON.stringify({
+                    user: {
+                        user: user,
+                        email: email,
+                        password: password
+                    }
+                })
+            }
+        };
+        return fetch(this.baseURL, configurationObject).then(res => res(json))
+    }
 }
