84ca3b7054c3b5d0680c15340466f9ee4e011298
Richard Cho <richardcho100@gmail.com>
2020-02-12 18:31:20 -0500

    Add loginURL & loginUser to post fetch login credentials

--- src/adapters/UsersAdapter.js
+++ src/adapters/UsersAdapter.js
@@ -1,6 +1,7 @@
 class UsersAdapter {
     constructor() {
         this.signupURL = "http://localhost:3000/signup"
+        this.loginURL = "http://localhost:3000/login"
     }
 
     signupUser(signupInfo) {
@@ -11,6 +12,18 @@ class UsersAdapter {
                 Accept: "application/json"
             },
             body: JSON.stringify(signupInfo)
+        })
+        .then(res => res.json())
+    }
+
+    loginUser(loginInfo) {
+        return fetch(this.loginURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(loginInfo)
         })
         .then(res => res.json())
     }
