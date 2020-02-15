b27034b4a9f1b3b30faba5d9a263c06516293ed4
Richard Cho <richardcho100@gmail.com>
2020-02-15 18:22:17 -0500

    Add borrow method to BooksAdapter to patch fetch book's availability

--- src/adapters/BooksAdapter.js
+++ src/adapters/BooksAdapter.js
@@ -6,4 +6,21 @@ class BooksAdapter {
     getBooks() {
         return fetch(this.baseURL).then(res => res.json())
     }
+
+    borrow(bookID, token) {
+        return fetch(`http://localhost:3000/books/${bookID}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                book: {
+                    id: bookID
+                }
+            })
+        })
+        .then(res => res.json())
+    }
 }
